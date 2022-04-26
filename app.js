b18d504626f1c0e06cb5191bc860998e03f446eb
Deepak878 <54785214+Deepak878@users.noreply.github.com>
2022-04-26 22:24:53 +0545

    second commit

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const path = require('path')
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
-const res = require("express/lib/response");
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const campground = require("./models/campground");
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser:true,
@@ -18,6 +18,7 @@ db.once("open", ()=>{
 
 const app = express();
 
+app.engine('ejs', ejsMate)
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname, 'views'))
