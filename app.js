79b0a4dd8d2ad958a6e5966dbe6dea521ec8d093
Deepak878 <54785214+Deepak878@users.noreply.github.com>
2022-05-17 18:31:11 +0545

    added security checks

--- app.js
+++ app.js
@@ -11,12 +11,15 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError')
 const passport = require('passport');
+const mongoSanitize = require('express-mongo-sanitize');
+
 const userRoutes = require('./routes/users');
 const LocalStratergy = require('passport-local');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const flash = require("connect-flash/lib/flash");
 const User = require('./models/user')
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser:true,
   useUnifiedTopology:true,
@@ -37,13 +40,17 @@ app.set('views',path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method')); 
 app.use(express.static(path.join(__dirname ,'public')));
-
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
 const sessionConfig = {
+    name:'session',
     secret: 'thisshouldbebettersecret!',
     resave: false,
     saveUnitialized: true,
     cookie : {
         httpOnly:true,
+        //secure:true for https after deployed
         expires: Date.now() + 1000*60*60*24*7,
         maxAge: 1000*60*60*24*7
     }
@@ -59,6 +66,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
+    console.log(req.query)
     console.log(req.session)
     res.locals.currentUser = req.user;
  res.locals.success = req.flash('success');
